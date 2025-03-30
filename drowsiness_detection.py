03542409367d4bf892862565e4eae82533bd5c81
pratham-bhoir <105415388+pratham-bhoir@users.noreply.github.com>
2025-03-30 15:58:36 +0530

    3rd commit

--- drowsiness_detection.py
+++ drowsiness_detection.py
@@ -8,4 +8,4 @@ image = cv2.imread('Images/drowsiness.png')  # Load input (coffee break) image
 image = cv2.resize(image, (100, 100))          # Resize image to 100x100
 
 pygame.mixer.init()
-pygame.mixer.music.load('siren.wav')
+pygame.mixer.music.load('siren.wav') #music loading
